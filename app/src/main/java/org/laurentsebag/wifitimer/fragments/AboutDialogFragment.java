190d0c5838edb8422e03fd7691a5b74ba57167db
Laurent Sebag <laurentsebag.dev@gmail.com>
2018-11-04 21:27:07 +0000

    Cleaning after removing tracking

--- app/src/main/java/org/laurentsebag/wifitimer/fragments/AboutDialogFragment.java
+++ app/src/main/java/org/laurentsebag/wifitimer/fragments/AboutDialogFragment.java
@@ -21,7 +21,6 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.text.method.LinkMovementMethod;
@@ -31,11 +30,6 @@ import android.widget.TextView;
 import org.laurentsebag.wifitimer.R;
 
 public class AboutDialogFragment extends DialogFragment {
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @NonNull
     @Override
